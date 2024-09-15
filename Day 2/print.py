96322e97cb47fa1247b167c0227016db0158b0ad
YukoHiroshi <vellakrishna9@gmail.com>
2024-09-15 11:37:16 -0400

    Print statements

---
+++ Day 2/print.py
@@ -0,0 +1,6 @@
+print("I am krishna")
+print("My age is 14")
+print("I like to play soccer")
+print("I also like to play basketball with my friends")
+print("I am in grade",9)
+print("/n My sister is named sudiksha")
