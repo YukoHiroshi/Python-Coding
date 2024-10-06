8c281dd215402540606abf77c8dfdba39bf1a88f
YukoHiroshi <vellakrishna9@gmail.com>
2024-10-06 11:45:49 -0400

    Percentage Calculation

---
+++ Day 4/Percentage calculation.py
@@ -0,0 +1,13 @@
+print("Enter your marks for 4 subjects:")
+math=int(input("math:"))
+english=int(input("english:"))
+science=int(input("science:"))
+history=int(input("history:"))
+
+sum=math+science+english+history
+
+print("total marks are",sum)
+
+average=(sum/400)*100
+
+print("total percentage is",average)
