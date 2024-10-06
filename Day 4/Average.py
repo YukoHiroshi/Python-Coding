7f082f9a0a6ba75130e5485300143232b004b905
YukoHiroshi <vellakrishna9@gmail.com>
2024-10-06 11:22:47 -0400

    Operator

---
+++ Day 4/Average.py
@@ -0,0 +1,13 @@
+krishna1=47
+krishna2=58
+krishna3=89
+krishna4=94
+krishna5=83
+
+sum=krishna1+krishna2+krishna3+krishna4+krishna5
+
+print("the sum of krishna is",sum)
+
+average=sum/5
+
+print("the avergae of krishna is",average)
